daa83dfa9e1019907873d82b2fa3ec5d1b2b7a6c
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-11-21 15:43:01 +0530

    Update app.js

--- RunningJSFromAScript/app.js
+++ RunningJSFromAScript/app.js
@@ -1,11 +1,20 @@
-let rating = 2;
-if (rating === 3) {
-    console.log("This is true");
-} else {
-    console.log("This is false");
-}
+// let rating = 2;
+// if (rating === 3) {
+//     console.log("This is true");
+// } else {
+//     console.log("This is false");
+// }
 
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Finally");
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Finally");
+// }
+const dayOfWeek = "Friday";
+
+if (dayOfWeek === "Monday") {
+    console.log("I hate mondays");
+} else if (dayOfWeek === "Sunday") {
+    console.log("Yay it's Sunday");
+} else if (dayOfWeek === "Friday") {
+    console.log("Yes it's Friday");
 }
