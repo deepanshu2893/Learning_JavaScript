18f6d7ca02b28abb831079a60f66e50c822dfe41
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-11-22 21:05:35 +0530

    Update app.js

--- LogicalOperators/app.js
+++ LogicalOperators/app.js
@@ -1,6 +1,16 @@
-const password = prompt("Enter your password");
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-    console.log("Valid Password")
-} else {
-    console.log("Incorrect Password")
+// const password = prompt("Enter your password");
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//     console.log("Valid Password")
+// } else {
+//     console.log("Incorrect Password")
+// }
+
+
+const age = 34;
+if (age < 5 || age >= 65) {
+    console.log("FREE");
+} else if (age < 10) {
+    console.log("$10");
+} else if (age < 65) {
+    console.log("$20");
 }
