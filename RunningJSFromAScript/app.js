231e9332223996d016e8e4ae152fd16cfafe918a
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-11-22 12:50:44 +0530

    Update app.js

--- RunningJSFromAScript/app.js
+++ RunningJSFromAScript/app.js
@@ -23,10 +23,13 @@
 const password = prompt("Please enter password");
 
 if (password.length >= 6) {
-    console.log("Password is fine")
+    if (password.indexOf(" ") === -1) {
+        console.log("Valid Password")
+    } else {
+        console.log("Password cannot contain spaces!")
+    }
 }
 else {
-    console.log("Password is too short");
-
+    console.log("Password is too short, must contain 6+ characters");
 }
 
