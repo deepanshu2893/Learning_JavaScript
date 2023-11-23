358b0c464320e78acb3cadc7852a192d3ab80eca
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-11-23 12:34:39 +0530

    Update app.js

--- LogicalOperators/app.js
+++ LogicalOperators/app.js
@@ -6,11 +6,16 @@
 // }
 
 
-const age = 6;
-if (age >= 0 && age < 5 || age >= 65) {
-    console.log("FREE");
-} else if (age >= 5 && age < 10) {
-    console.log("$10");
-} else if (age >= 10 && age < 65) {
-    console.log("$20");
-}
+// const age = 6;
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log("FREE");
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10");
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20");
+// }
+
+const firstName = prompt("Enter your first name");
+if (!firstName) {
+    console.log("First Name");
+} 
