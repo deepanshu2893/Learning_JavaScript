357f629f191f00f06628621b9aee4d82ba8760ad
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-11-23 12:45:09 +0530

    Update app.js

--- SwitchStatement/app.js
+++ SwitchStatement/app.js
@@ -1,4 +1,20 @@
-const day = 2;
-if (day === 1) {
-    console.log("MONDAY")
+const day = 5;
+switch (day) {
+    case 1:
+        console.log("MONDAY")
+        break
+    case 2:
+        console.log("TUESDAY")
+        break
+    case 3:
+        console.log("WEDNESDAY")
+        break
+    case 4:
+        console.log("THURSDAY")
+        break
+    case 5:
+        console.log("FRIDAY")
+        break
+    default:
+        console.log("INVALID NUMBER")
 }
