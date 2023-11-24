8b9b9d45b1e6b8c6dc9656024f65e41ca507bfe5
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-11-24 13:43:38 +0530

    arrays

---
+++ Arrays/app.js
@@ -0,0 +1,15 @@
+let array = [1, 2, 3, 4, 5];
+console.log(array);
+
+console.log(array.pop())
+console.log(array.push(6))
+console.log(array.shift())
+console.log(array.unshift(0))
+
+console.log(array);
+
+let cat = ["blue", "kitkat", "Orange"];
+let dog = ["black", "white", "yellow"];
+
+let cots = cat.concat(dog);
+console.log(cots);
