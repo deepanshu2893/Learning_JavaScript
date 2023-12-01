67907b9c48d618ffd05b1b3f5bcf23c6ec452bc2
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-12-01 22:21:12 +0530

    Update app.js

--- Loops/WhileLoops/app.js
+++ Loops/WhileLoops/app.js
@@ -9,13 +9,23 @@
 // console.log(9)
 // console.log(10)
 
-let count = 0;
-while (count < 10) {
-    console.log(count);
-    count++;
-}
+// let count = 0;
+// while (count < 10) {
+//     console.log(count);
+//     count++;
+// }
 
-let login = prompt("Enter your name");
-while (login != "Deepanshu") {
-    login = prompt("Name is wrong");
+// let login = prompt("Enter your name");
+// while (login != "Deepanshu") {
+//     login = prompt("Name is wrong");
+// }
+
+let input = prompt("Hey, say something")
+while (true) {
+    input = prompt(input);
+    if (input.toLowerCase() === "stop copying me") {
+        break;
+    }
+
 }
+console.log("You win")
