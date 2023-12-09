e5944b8abf1fc07f14c9d2866076986e958b20c5
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-12-09 14:14:31 +0530

    Update app.js

--- Functions/Scope/app.js
+++ Functions/Scope/app.js
@@ -1,4 +1,14 @@
-function collectEggs() {
-    let totalEggs = 6;
-    console.log(totalEggs);
+// function collectEggs() {
+//     let totalEggs = 6;
+//     console.log(totalEggs);
+// }
+
+
+let radius = 8;
+if (radius > 0) {
+    const PI = 3.14159;
+    let msg = "HIIII!"
 }
+
+console.log(radius);
+console.log(PI); 
