22471adf38ea83315db24bd4ac2811a910f5c4a7
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-12-09 15:29:56 +0530

    Update app.js

--- Functions/Scope/app.js
+++ Functions/Scope/app.js
@@ -3,12 +3,23 @@
 //     console.log(totalEggs);
 // }
 
+// Block Scope
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = "HIIII!"
+// }
 
-let radius = 8;
-if (radius > 0) {
-    const PI = 3.14159;
-    let msg = "HIIII!"
+// console.log(radius);
+// console.log(PI);
+
+// Lexical Scope
+
+function hello() {
+    const heroes = ["Superman", "Batman", "Spiderman"];
+    function help() {
+        for (let hero of heroes) {
+            console.log(`Help, ${hero}`);
+        }
+    }
+    help();
 }
-
-console.log(radius);
-console.log(PI); 
