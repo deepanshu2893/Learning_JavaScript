65d92b71ce59b6016cf999f0ca4f8ae625003bb2
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-12-10 19:36:09 +0530

    Update app.js

--- Functions/Methods/app.js
+++ Functions/Methods/app.js
@@ -1,9 +1,26 @@
-const myMath = {
-    PI: 3.14159,
-    square: function (num) {
-        return num * num;
-    },
-    cube: function (num) {
-        return num ** 3;
+// const myMath = {
+//     PI: 3.14159,
+//     square: function (num) {
+//         return num * num;
+//     },
+//     cube: function (num) {
+//         return num ** 3;
+
+
+//     }
+// }
+
+
+const cat = {
+    name: "Deepanshu",
+    breed: "Human",
+    color: "Indian",
+
+
+    hello() {
+        console.log(this.color + "asdsad");
     }
 }
+
+const hello2 = cat.hello;
+
