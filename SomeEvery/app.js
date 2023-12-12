89dd1be3fd1b4958037dd0a28246dbea5b7b7197
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-12-12 17:05:48 +0530

    some

---
+++ SomeEvery/app.js
@@ -0,0 +1,14 @@
+const exams = [80, 98, 92, 78, 77, 90, 89, 84, 81, 77]
+
+exams.every(score => score >= 75);
+
+const movies = [{
+    title: "Amadeus",
+    score: 98,
+    year: 1984
+},
+{
+    title: "Sharknado",
+    score: 35,
+    year: 2013
+}]
