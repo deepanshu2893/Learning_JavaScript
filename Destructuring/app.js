e6e5005a2d1e5ce0309098f144b4ddb8983f935e
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-12-15 14:44:05 +0530

    destructuring

---
+++ Destructuring/app.js
@@ -0,0 +1,16 @@
+// const scores = [929321, 89931, 99123, 912801238, 478391283, 7394172394, 123213];
+
+// const highScore = scores[0];
+// const secondHighScore = scores[1]; 
+
+// const [gold, silver] = scores; 
+
+
+const user = {
+    username: "Deepanshu Panwar",
+    email: "deepanshu12345@gmail.com",
+    phone: 1234567890
+
+}
+
+const { username } = user; 
--- Rest/app.js
+++ Rest/app.js
@@ -1,3 +1,4 @@
 function sum() {
     console.log(arguments);
 }
+
